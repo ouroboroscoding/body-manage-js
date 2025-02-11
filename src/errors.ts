8a40e9e1585912dafc0a8cedd9a94073f1391fad
Ouroboros Coding Inc <chris@ouroboroscoding.com>
2025-02-11 11:37:10 -0500

    Development - Updated portal definition - Added errors file - Added exporting of errors from index

---
+++ src/errors.ts
@@ -0,0 +1,15 @@
+/**
+ * Errors
+ *
+ * Contains constants to describe errors from the manage service
+ *
+ * @author Chris Nasr <chris@ouroboroscoding.com>
+ * @copyright Ouroboros Coding Inc.
+ * @created 2025-02-11
+ */
+
+// Re-export the body errors
+export { errors as body } from '@ouroboros/body';
+
+// Constants
+export const SHELL_ISSUE = 1500
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@
 // Ouroboros modules
 import { Service } from '@ouroboros/body';
 
+// Import and re-export module files
+export * as errors from './errors';
+
 // Create an instance of Service and export it as default
 const manage = new Service('manage');
 export default manage;
